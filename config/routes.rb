2db10fabce35ec81129fa6b7d20dec442b287673
shar0ff <sharovivan17@gmail.com>
2024-09-16 23:17:05 +0200

    Add a Blog Post edit, update and destroy actions. Minor refactor.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
 
-  get 'blog_posts/new', to: 'blog_posts#new', as: 'new_blog_post'
-  get '/blog_posts/:id', to: 'blog_posts#show', as: 'blog_post'
-
-  post '/blog_posts', to: 'blog_posts#create', as: 'blog_posts'
+  resources :blog_posts
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
