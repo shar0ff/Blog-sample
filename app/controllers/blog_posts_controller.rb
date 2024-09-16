b850b2de094234a256943cd484df740ad1ab9702
shar0ff <sharovivan17@gmail.com>
2024-09-16 19:18:59 +0200

    Add a Blog Post new action. Add some validations to model.

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -8,4 +8,24 @@ class BlogPostsController < ApplicationController
   rescue ActiveRecord::RecordNotFound
     redirect_to root_path
   end
+
+  def new
+    @blog_post = BlogPost.new
+  end
+
+  def create
+    @blog_post = BlogPost.new(blog_post_params)
+    if @blog_post.save
+      redirect_to @blog_post
+    else
+      # 422 = :unprocessable_entity
+      render 'new', status: 422
+    end
+  end
+
+  private
+
+  def blog_post_params
+    params.require(:blog_post).permit(:title, :body)
+  end
 end
