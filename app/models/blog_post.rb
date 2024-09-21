eeb07529037e143029016c8069c47473cac8fb2e
shar0ff <sharovivan17@gmail.com>
2024-09-21 17:53:56 +0200

    Move blog posts body to action text

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -1,10 +1,10 @@
 class BlogPost < ApplicationRecord
+  has_rich_text :content
 
   validates :title, presence: true
-  validates :body, presence: true
+  validates :content, presence: true
 
-  # = scope :sorted, -> { order(arel_table(:published_at).desc.nulls_last).order(updated_at: :desc }
-  scope :sorted, -> { order(Arel.sql(("published_at: DESC NULL LAST").order(updated_at: :desc))) }
+  scope :sorted, -> { order(arel_table[:published_at].desc.nulls_last).order(updated_at: :desc) }
   scope :draft, -> { where(published_at: nil) }
   scope :published, -> { where("published_at <= ?", Time.current) }
   scope :scheduled, -> { where("published_at > ?", Time.current) }
