c6842c833a52c87080f16dae78b633e2bd52848d
shar0ff <sharovivan17@gmail.com>
2024-09-29 22:51:52 +0200

    Add upload cover image functionality to blog posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :blog_posts
+  resources :blog_posts do
+    resource :cover_image, module: :blog_posts, only: [:destroy]
+  end
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
